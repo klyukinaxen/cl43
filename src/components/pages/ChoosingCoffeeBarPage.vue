267183edfb1a6e6f356ed18ccf203b5ed4c04dbe
Xenia Klyukina <xenia.klyukina@gmail.com>
2021-03-13 12:41:08 +0300

    fix

--- src/components/pages/ChoosingCoffeeBarPage.vue
+++ src/components/pages/ChoosingCoffeeBarPage.vue
@@ -14,32 +14,13 @@
 
 <script>
 import Cookies from 'js-cookie'
-// import coffeeBarData from '@/components/CoffeeBarInfo.json'
+
+import coffeeBarData from '@/components/CoffeeBarInfo.json'
+
 export default {
     data () {
         return {
-            сoffeeBars: [
-                { name: 'Администрация', id: '1' },
-                { name: 'Атлант', id: '2' },
-                { name: 'Глобус', id: '3' },
-                { name: 'ГринХаус', id: '4' },
-                { name: 'Джем', id: '5' },
-                { name: 'ЖД', id: '6' },
-                { name: 'Кейтеринг', id: '7' },
-                { name: 'Колизей', id: '8' },
-                { name: 'Макси', id: '9' },
-                { name: 'Малахит', id: '10' },
-                { name: 'Московская', id: '11' },
-                { name: 'Октябрь', id: '12' },
-                { name: 'Простор', id: '13' },
-                { name: 'Спасская', id: '14' },
-                { name: 'Спортмастер', id: '15' },
-                { name: 'Театралка', id: '16' },
-                { name: 'Цирк', id: '17' },
-                { name: 'Филармония', id: '18' },
-                { name: 'Фудтрак', id: '19' },
-                { name: 'ЮЗ', id: '20' }
-            ]
+            сoffeeBars: coffeeBarData.coffeeBars
         }
     },
     methods: {
